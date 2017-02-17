c6e96267bd3b6a536c40b27bbaacd42ab31b36a2
panickylemon <panickylemon>
2017-02-17 16:53:40 +0300

    category

--- wp-content/themes/rdrill-theme/woocommerce/content-single-product-machine.php
+++ wp-content/themes/rdrill-theme/woocommerce/content-single-product-machine.php
@@ -68,16 +68,14 @@ if ( ! defined( 'ABSPATH' ) ) {
         <h2>Основные преимущества станка <?php echo $product->get_attribute( 'name-category' ); ?></h2>
 
         <!-- видео -->
-
-
         <div class="video-container">
             <div class="video-wrapper">
-                <div id="player" data-video-id="ca6ARA4V0v4"></div>
+                <div id="player" data-video-id="<?php echo get_post_meta( $post->ID, 'video_machine', true ); ?>"></div>
                 <div id="thumbnail_container" class="thumbnail_container">
                     <img class="thumbnail" id="thumbnail"
-                         src="http://img.youtube.com/vi/ca6ARA4V0v4/sddefault.jpg" alt="превью"/>
+                         src="http://img.youtube.com/vi/<?php echo get_post_meta( $post->ID, 'video_machine', true ); ?>/sddefault.jpg" alt="превью"/>
                 </div>
-                <a class="start-video"><img src="image/icons/play.png" alt="play">
+                <a class="start-video"><img src="/wp-content/themes/rdrill-theme/image/icons/play.png" alt="play">
                 </a>
             </div>
         </div>
