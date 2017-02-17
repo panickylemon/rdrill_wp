1f1113a8ac4728bdcffb335a57b054441c7a838c
panickylemon <alenka__1992@mail.ru>
2017-02-17 23:44:05 +0300

    edit

--- wp-content/themes/rdrill-theme/woocommerce/content-single-product-machine.php
+++ wp-content/themes/rdrill-theme/woocommerce/content-single-product-machine.php
@@ -119,22 +119,21 @@ if ( ! defined( 'ABSPATH' ) ) {
     </div> <!-- end l-container -->
 
         <!-- Дополнительная инфа -->
-
     <?php $tabs = apply_filters( 'woocommerce_product_tabs', array() );?>
     <?php $tab = $tabs['additional_information'] ?>
-    <div class="woocommerce-Tabs-panel woocommerce-Tabs-panel--<?php echo esc_attr( $key ); ?> panel entry-content wc-tab" id="tab-<?php echo esc_attr( $key ); ?>">
+    <div>
         <?php call_user_func( $tab['callback'], $key, $tab ); ?>
     </div>
 
         <!-- Отзывы -->
         <?php $tab = $tabs['reviews'] ?>
-    <div class="woocommerce-Tabs-panel woocommerce-Tabs-panel--<?php echo esc_attr( $key ); ?> panel entry-content wc-tab" id="tab-<?php echo esc_attr( $key ); ?>">
+    <div>
         <?php call_user_func( $tab['callback'], $key, $tab ); ?>
     </div>
 
         <!-- Описание -->
     <?php $tab = $tabs['description'] ?>
-    <div class="woocommerce-Tabs-panel woocommerce-Tabs-panel--<?php echo esc_attr( $key ); ?> panel entry-content wc-tab" id="tab-<?php echo esc_attr( $key ); ?>">
+    <div>
         <?php call_user_func( $tab['callback'], $key, $tab ); ?>
     </div>
 
