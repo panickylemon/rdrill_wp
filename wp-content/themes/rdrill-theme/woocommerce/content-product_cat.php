f0216e918c9117ba8e7c57686306988362bb873a
panickylemon <panickylemon>
2017-02-22 15:11:09 +0300

    category

--- wp-content/themes/rdrill-theme/woocommerce/content-product_cat.php
+++ wp-content/themes/rdrill-theme/woocommerce/content-product_cat.php
@@ -1,39 +1,189 @@
+<?php
+/**
+ * The template for displaying product category thumbnails within loops
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/content-product_cat.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ * @author  WooThemes
+ * @package WooCommerce/Templates
+ * @version 2.6.1
+ */
 
-<div class="zzxx">
-    <li <?php wc_product_cat_class( '', $category ); ?>>
-        <?php
-        /**
-         * woocommerce_before_subcategory hook.
-         *
-         * @hooked woocommerce_template_loop_category_link_open - 10
-         */
-        do_action( 'woocommerce_before_subcategory', $category );
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+?>
 
-        /**
-         * woocommerce_before_subcategory_title hook.
-         *
-         * @hooked woocommerce_subcategory_thumbnail - 10
-         */
-        do_action( 'woocommerce_before_subcategory_title', $category );
+<div class="about-page__wrap-menu clearfix">
+    <div class="about-navigation__item about-navigation__item-first">
+        <span class="delimiter-about delimiter-about-1"></span>
+        <span class="delimiter-about delimiter-about-2"></span>
+        <span class="delimiter-about delimiter-about-3"></span>
+        <span class="delimiter-about delimiter-about-4"></span>
+        <a class="about-navigation__link" href="#">
+            <div class="about-navigation__image about-navigation__image-brand"></div>
+            <p class="about-navigation__title">
+                О бренде Rotabroach
+            </p>
+            <span class="delimiter-about-link delimiter-about-link-1"></span>
+            <span class="delimiter-about-link delimiter-about-link-2"></span>
+            <span class="delimiter-about-link delimiter-about-link-3"></span>
+            <span class="delimiter-about-link delimiter-about-link-4"></span>
+        </a>
+    </div>
+    <div class="about-navigation__item about-navigation__item-middle">
+        <span class="delimiter-about delimiter-about-1"></span>
+        <span class="delimiter-about delimiter-about-2"></span>
+        <span class="delimiter-about delimiter-about-3"></span>
+        <span class="delimiter-about delimiter-about-4"></span>
+        <a class="about-navigation__link" href="#">
+            <div class="about-navigation__image about-navigation__image-quality"></div>
+            <p class="about-navigation__title">
+                Качество Rotabroach
+            </p>
+            <span class="delimiter-about-link delimiter-about-link-1"></span>
+            <span class="delimiter-about-link delimiter-about-link-2"></span>
+            <span class="delimiter-about-link delimiter-about-link-3"></span>
+            <span class="delimiter-about-link delimiter-about-link-4"></span>
+        </a>
+    </div>
+    <div class="about-navigation__item about-navigation__item-third">
+        <span class="delimiter-about delimiter-about-1"></span>
+        <span class="delimiter-about delimiter-about-2"></span>
+        <span class="delimiter-about delimiter-about-3"></span>
+        <span class="delimiter-about delimiter-about-4"></span>
+        <a class="about-navigation__link" href="#">
+            <div class="about-navigation__image about-navigation__image-technologies"></div>
+            <p class="about-navigation__title">
+                Технологии Custmart
+            </p>
+            <span class="delimiter-about-link delimiter-about-link-1"></span>
+            <span class="delimiter-about-link delimiter-about-link-2"></span>
+            <span class="delimiter-about-link delimiter-about-link-3"></span>
+            <span class="delimiter-about-link delimiter-about-link-4"></span>
+        </a>
+    </div>
 
-        /**
-         * woocommerce_shop_loop_subcategory_title hook.
-         *
-         * @hooked woocommerce_template_loop_category_title - 10
-         */
-        do_action( 'woocommerce_shop_loop_subcategory_title', $category );
+    <div class="about-navigation__item about-navigation__item-first">
+        <span class="delimiter-about delimiter-about-1"></span>
+        <span class="delimiter-about delimiter-about-2"></span>
+        <span class="delimiter-about delimiter-about-3"></span>
+        <span class="delimiter-about delimiter-about-4"></span>
+        <a class="about-navigation__link" href="#">
+            <div class="about-navigation__image about-navigation__image-guarantee"></div>
+            <p class="about-navigation__title">
+                Гарантия
+            </p>
+            <span class="delimiter-about-link delimiter-about-link-1"></span>
+            <span class="delimiter-about-link delimiter-about-link-2"></span>
+            <span class="delimiter-about-link delimiter-about-link-3"></span>
+            <span class="delimiter-about-link delimiter-about-link-4"></span>
+        </a>
+    </div>
+    <div class="about-navigation__item about-navigation__item-middle">
+        <span class="delimiter-about delimiter-about-1"></span>
+        <span class="delimiter-about delimiter-about-2"></span>
+        <span class="delimiter-about delimiter-about-3"></span>
+        <span class="delimiter-about delimiter-about-4"></span>
+        <a class="about-navigation__link" href="#">
+            <div class="about-navigation__image about-navigation__image-news"></div>
+            <p class="about-navigation__title">
+                Новости
+            </p>
+            <span class="delimiter-about-link delimiter-about-link-1"></span>
+            <span class="delimiter-about-link delimiter-about-link-2"></span>
+            <span class="delimiter-about-link delimiter-about-link-3"></span>
+            <span class="delimiter-about-link delimiter-about-link-4"></span>
+        </a>
+    </div>
+    <div class="about-navigation__item about-navigation__item-third">
+        <span class="delimiter-about delimiter-about-1"></span>
+        <span class="delimiter-about delimiter-about-2"></span>
+        <span class="delimiter-about delimiter-about-3"></span>
+        <span class="delimiter-about delimiter-about-4"></span>
+        <a class="about-navigation__link" href="#">
+            <div class="about-navigation__image about-navigation__image-representation"></div>
+            <p class="about-navigation__title">
+                Представительство в России
+            </p>
+            <span class="delimiter-about-link delimiter-about-link-1"></span>
+            <span class="delimiter-about-link delimiter-about-link-2"></span>
+            <span class="delimiter-about-link delimiter-about-link-3"></span>
+            <span class="delimiter-about-link delimiter-about-link-4"></span>
+        </a>
+    </div>
 
-        /**
-         * woocommerce_after_subcategory_title hook.
-         */
-        do_action( 'woocommerce_after_subcategory_title', $category );
+    <div class="about-navigation__item about-navigation__item-first">
+        <span class="delimiter-about delimiter-about-1"></span>
+        <span class="delimiter-about delimiter-about-2"></span>
+        <span class="delimiter-about delimiter-about-3"></span>
+        <span class="delimiter-about delimiter-about-4"></span>
+        <a class="about-navigation__link" href="#">
+            <div class="about-navigation__image about-navigation__image-review"></div>
+            <p class="about-navigation__title">
+                Отзывы клиентов
+            </p>
+            <span class="delimiter-about-link delimiter-about-link-1"></span>
+            <span class="delimiter-about-link delimiter-about-link-2"></span>
+            <span class="delimiter-about-link delimiter-about-link-3"></span>
+            <span class="delimiter-about-link delimiter-about-link-4"></span>
+        </a>
+    </div>
+    <div class="about-navigation__item about-navigation__item-middle">
+        <span class="delimiter-about delimiter-about-1"></span>
+        <span class="delimiter-about delimiter-about-2"></span>
+        <span class="delimiter-about delimiter-about-3"></span>
+        <span class="delimiter-about delimiter-about-4"></span>
+        <a class="about-navigation__link" href="#">
+            <div class="about-navigation__image about-navigation__image-conditions"></div>
+            <p class="about-navigation__title">
+                Условия сотрудничества
+            </p>
+            <span class="delimiter-about-link delimiter-about-link-1"></span>
+            <span class="delimiter-about-link delimiter-about-link-2"></span>
+            <span class="delimiter-about-link delimiter-about-link-3"></span>
+            <span class="delimiter-about-link delimiter-about-link-4"></span>
+        </a>
+    </div>
+    <div class="about-navigation__item about-navigation__item-third">
+        <span class="delimiter-about delimiter-about-1"></span>
+        <span class="delimiter-about delimiter-about-2"></span>
+        <span class="delimiter-about delimiter-about-3"></span>
+        <span class="delimiter-about delimiter-about-4"></span>
+        <a class="about-navigation__link" href="#">
+            <div class="about-navigation__image about-navigation__image-news"></div>
+            <p class="about-navigation__title">
+                Статьи
+            </p>
+            <span class="delimiter-about-link delimiter-about-link-1"></span>
+            <span class="delimiter-about-link delimiter-about-link-2"></span>
+            <span class="delimiter-about-link delimiter-about-link-3"></span>
+            <span class="delimiter-about-link delimiter-about-link-4"></span>
+        </a>
+    </div>
 
-        /**
-         * woocommerce_after_subcategory hook.
-         *
-         * @hooked woocommerce_template_loop_category_link_close - 10
-         */
-        do_action( 'woocommerce_after_subcategory', $category ); ?>
-    </li>
+    <div class="about-navigation__item about-navigation__item-first">
+        <span class="delimiter-about delimiter-about-1"></span>
+        <span class="delimiter-about delimiter-about-2"></span>
+        <span class="delimiter-about delimiter-about-3"></span>
+        <span class="delimiter-about delimiter-about-4"></span>
+        <a class="about-navigation__link" href="#">
+            <div class="about-navigation__image about-navigation__image-sale"></div>
+            <p class="about-navigation__title">
+                Акции
+            </p>
+            <span class="delimiter-about-link delimiter-about-link-1"></span>
+            <span class="delimiter-about-link delimiter-about-link-2"></span>
+            <span class="delimiter-about-link delimiter-about-link-3"></span>
+            <span class="delimiter-about-link delimiter-about-link-4"></span>
+        </a>
+    </div>
 </div>
 
