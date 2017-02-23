fbea29acd6578166ef2fcffcfc82375320db6d7e
panickylemon <alenka__1992@mail.ru>
2017-02-23 16:43:01 +0300

    edit

--- wp-content/themes/rdrill-theme/catalog-page.php
+++ wp-content/themes/rdrill-theme/catalog-page.php
@@ -9,9 +9,7 @@
 jhngbfvdc
 <main class="content zz">
 	<div class="l-container cc">
-		<?php if (have_posts()): while (have_posts()): the_post(); ?>
-			<?php the_content(); ?>
-		<?php endwhile; endif; ?>
+		ggggggggggggggggggggggggggggggggg
 	</div>
 </main>
 
