7a0aba3e9d8a7d468b6ee47df67c684fbfb2a679
panickylemon <alenka__1992@mail.ru>
2017-02-23 16:49:41 +0300

    edit

--- wp-content/themes/rdrill-theme/template/catalog-page.php
+++ wp-content/themes/rdrill-theme/template/catalog-page.php
@@ -6,15 +6,5 @@
 
 
 <?php get_header(); ?>
-jhngbfvdc
-<main class="content zz">
-	<div class="l-container cc">
-		<section>
-			<?php if (have_posts()): while (have_posts()): the_post(); ?>
-				<?php the_content(); ?>
-			<?php endwhile; endif; ?>
-		</section>
-	</div>
-</main>
 
-<?php get_footer(); ?>
+
