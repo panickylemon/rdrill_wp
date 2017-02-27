42d34ed9330cde42d9374a84817e82ac1aeca539
panickylemon <panickylemon>
2017-02-27 14:36:28 +0300

    category

---
+++ wp-content/themes/rdrill-theme/template/about-page.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: About Page
+ */
+?>
+
+
+<?php get_header(); ?>
+
+<main class="content">
+	<?php if (have_posts()): while (have_posts()): the_post(); ?>
+		<?php the_content(); ?>
+	<?php endwhile; endif; ?>
+</main>
+
+<?php get_footer(); ?>
+
+
