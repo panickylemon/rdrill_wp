4c8c745695f04ddd3eb4b83acc8671b048b910e9
panickylemon <alenka__1992@mail.ru>
2017-02-27 23:32:55 +0300

    edit

--- wp-content/themes/rdrill-theme/js/main.js
+++ wp-content/themes/rdrill-theme/js/main.js
@@ -159,7 +159,7 @@ $(document).ready(function () {
     $('.table-responsive').scroll( function() {
         initSticky()
     });
-    $(".tablesorter").tablesorter({ widgets: ['zebra','staticRow'] });
+    $(".tablesorter").tablesorter({ widgets: ['staticRow'] });
     initSticky();
 
     $('.card-slider').bxSlider({
