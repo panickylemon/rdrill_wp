5c332bb663a6ea8d01569c8cc189b3dd4f520629
panickylemon <panickylemon>
2017-02-27 14:42:13 +0300

    category

--- wp-content/themes/rdrill-theme/functions.php
+++ wp-content/themes/rdrill-theme/functions.php
@@ -52,7 +52,7 @@ if(function_exists('register_nav_menus')){
 	register_nav_menus(
 			array( // создаём любое количество областей
 					'main_menu' => 'Главное меню', // 'имя' => 'описание'
-					'foot_menu' => 'Меню в футере'
+					'foot_menu' => 'Меню для дочерних > О компании'
 			)
 	);
 }
