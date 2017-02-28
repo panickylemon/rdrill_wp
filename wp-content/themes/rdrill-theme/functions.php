6ed6567316e3b3271faf204db192f3c3503b8923
panickylemon <panickylemon>
2017-02-28 12:05:51 +0300

    category

--- wp-content/themes/rdrill-theme/functions.php
+++ wp-content/themes/rdrill-theme/functions.php
@@ -131,7 +131,7 @@ class asideMenuWalker extends Walker_Nav_Menu
         $attributes = !empty( $item->url ) ? ' href="' .esc_attr($item->url). '"' : '';
         $item_output = $args->before;
         //всем ссылкам первого уровня
-        if ($depth == 1) {
+        if ($depth == 2) {
             $item_output.= '<a class = "about-sidebar__list-item"'. $attributes .'>'.$item->title.'</a>';
         }
 
