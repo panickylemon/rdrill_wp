7e46193f35a07d80961c408151b683e5d9733843
panickylemon <panickylemon>
2017-02-28 17:25:05 +0300

    category

--- wp-content/themes/rdrill-theme/single.php
+++ wp-content/themes/rdrill-theme/single.php
@@ -182,6 +182,43 @@
     <?php endif; ?>
     </div>
 
+    <div class="relatedposts">
+        <h3>Похожие записи</h3>
+        <?php
+        $orig_post = $post;
+        global $post;
+        $tags = wp_get_post_tags($post->ID);
+
+        if ($tags) {
+            $tag_ids = array();
+            foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;
+            $args=array(
+                'tag__in' => $tag_ids,
+                'post__not_in' => array($post->ID),
+                'posts_per_page'=>4, // Количество записей, которые выводятся
+                'caller_get_posts'=>1
+            );
+
+            $my_query = new wp_query( $args );
+
+            while( $my_query->have_posts() ) {
+                $my_query->the_post();
+                ?>
+
+                <div class="relatedthumb">
+                    <a rel="external" href="<? the_permalink()?>"><?php the_post_thumbnail(array(150,100)); ?><br />
+                        <?php the_title(); ?>
+                    </a>
+                </div>
+
+            <? }
+        }
+        $post = $orig_post;
+        wp_reset_query();
+        ?>
+    </div>
+
+
     <div class="l-container other-news">
         <p class="other-news__title">Рекомендуем почитать дополнительно:</p>
         <div class="other-news__wrap clearfix">
