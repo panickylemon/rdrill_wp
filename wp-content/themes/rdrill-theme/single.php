f2de3c3c72eb3160da9027f13d798712edb358cc
panickylemon <panickylemon>
2017-02-28 15:45:41 +0300

    category

---
+++ wp-content/themes/rdrill-theme/single.php
@@ -0,0 +1,21 @@
+<?php if ( have_posts() ) : ?>
+    <?php while ( have_posts() ) : the_post();?>
+
+        <div class="post">
+            <h2 class="title-post"><?php the_title(); ?></h2>
+
+            <?php if ( has_post_thumbnail()) : ?>
+                <div class="post-thumb">
+                    <a href="<?php the_permalink() ?>"><?php the_post_thumbnail(); ?></a>
+                </div>
+            <?php endif; ?>
+
+            <?php the_content(''); ?>
+
+        </div>
+
+        <?php comments_template();?>
+
+    <?php endwhile; ?>
+
+<?php endif; ?>
