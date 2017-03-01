bc275927185e0824af83352591857362ee894c50
panickylemon <alenka__1992@mail.ru>
2017-03-01 23:58:20 +0300

    edit

--- wp-content/themes/rdrill-theme/woocommerce/archive-product-drills.php
+++ wp-content/themes/rdrill-theme/woocommerce/archive-product-drills.php
@@ -62,12 +62,11 @@ do_action('woocommerce_before_main_content');
     </div>
 
     <div class="l-container">
-        <?php echo do_shortcode('[br_filters attribute=price type=checkbox]');?>
         <?php
-        //echo do_shortcode( '[woof sid="auto_shortcode" autohide=0 taxonomies=product_cat:8 ]' );
+        echo do_shortcode( '[woof sid="auto_shortcode" autohide=0 taxonomies=product_cat:8 ]' );
         ?>
 <?php
-//echo do_shortcode( '[woof_products per_page=8 columns=3 is_ajax=0 taxonomies=product_cat:8]' );
+echo do_shortcode( '[woof_products per_page=8 columns=3 is_ajax=0 taxonomies=product_cat:8]' );
 ?>
 
         <div class="button-show-filter"><span>Показать фильтр</span></div>
