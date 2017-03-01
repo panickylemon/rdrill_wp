5e4c36fb90362ca46ccddc1aca47e290a3942bb5
panickylemon <panickylemon>
2017-03-01 13:07:04 +0300

    category

--- wp-content/themes/rdrill-theme/template/base-page.php
+++ wp-content/themes/rdrill-theme/template/base-page.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Основной шаблон (без сайбдара)
+ * Template Name: Основной шаблон (без сайдбара)
  */
 ?>
 
@@ -12,6 +12,7 @@
         <?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
     </div>
     <div class="l-container">
+        <h1><?php the_title(); ?></h1>
         <?php if (have_posts()): while (have_posts()): the_post(); ?>
             <?php the_content(); ?>
         <?php endwhile; endif; ?>
