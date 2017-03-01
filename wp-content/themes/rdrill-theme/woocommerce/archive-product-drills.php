3abc683e96300b7850999897cc290f6600ed012f
panickylemon <alenka__1992@mail.ru>
2017-03-01 23:59:24 +0300

    edit

--- wp-content/themes/rdrill-theme/woocommerce/archive-product-drills.php
+++ wp-content/themes/rdrill-theme/woocommerce/archive-product-drills.php
@@ -66,7 +66,7 @@ do_action('woocommerce_before_main_content');
         echo do_shortcode( '[woof sid="auto_shortcode" autohide=0 taxonomies=product_cat:8 ]' );
         ?>
 <?php
-echo do_shortcode( '[woof_products per_page=8 columns=3 is_ajax=0 taxonomies=product_cat:8]' );
+echo do_shortcode( '[woof_products per_page=8 columns=3 is_ajax=1 taxonomies=product_cat:8]' );
 ?>
 
         <div class="button-show-filter"><span>Показать фильтр</span></div>
