5811f88c222b8cabf2eef5808f3c48ea308d090a
panickylemon <panickylemon>
2017-03-02 13:08:20 +0300

    category

---
+++ wp-content/themes/rdrill-theme/searchform.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+    <main class="content">
+        <div class="l-container">
+            <?php if (function_exists('dimox_breadcrumbs')) dimox_breadcrumbs(); ?>
+        </div>
+
+        <div class="contacts-wrap">
+            <div class="l-container">
+
+                <?php if (have_posts()): while (have_posts()): the_post(); ?>
+                    <?php the_content(); ?>
+                <?php endwhile; endif; ?>
+            </div>
+        </div>
+    </main>
+
+<?php get_footer(); ?>
