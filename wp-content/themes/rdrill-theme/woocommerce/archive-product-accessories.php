393ad157dc96a2a6b750755c927e1000cb0284b4
panickylemon <alenka__1992@mail.ru>
2017-03-02 21:50:12 +0300

    edit

--- wp-content/themes/rdrill-theme/woocommerce/archive-product-accessories.php
+++ wp-content/themes/rdrill-theme/woocommerce/archive-product-accessories.php
@@ -63,13 +63,13 @@ do_action('woocommerce_before_main_content');
 
     <div class="l-container">
         <?php
-        echo do_shortcode( '[woof sid="auto_shortcode" autohide=0 autosubmit=0 taxonomies=product_cat:8 ]' );
+        echo do_shortcode( '[woof sid="auto_shortcode" autohide=0 autosubmit=0 taxonomies=product_cat:10 ]' );
         ?>
         <?php dynamic_sidebar( 'sidebar-1' ); ?>
 
         <?php
 
-        echo do_shortcode( '[woof_products per_page=8 columns=3 is_ajax=1 taxonomies=product_cat:8]' );
+        echo do_shortcode( '[woof_products per_page=8 columns=3 is_ajax=1 taxonomies=product_cat:10]' );
         ?>
         <div class="button-show-filter"><span>Показать фильтр</span></div>
         <div class="filter-sidebar">
