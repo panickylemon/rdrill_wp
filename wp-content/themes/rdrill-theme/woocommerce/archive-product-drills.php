33841574c00ba5137d71224f5ffcadf340d2cfc9
panickylemon <panickylemon>
2017-03-02 12:33:04 +0300

    category

--- wp-content/themes/rdrill-theme/woocommerce/archive-product-drills.php
+++ wp-content/themes/rdrill-theme/woocommerce/archive-product-drills.php
@@ -66,7 +66,10 @@ do_action('woocommerce_before_main_content');
        // echo do_shortcode( '[woof sid="auto_shortcode" autohide=0 autosubmit=0 taxonomies=product_cat:8 ]' );
         ?>
         <?php dynamic_sidebar( 'sidebar-1' ); ?>
+
+        <?php get_search_form(); ?>
 <?php
+
 echo do_shortcode( '[woof_products per_page=8 columns=3 is_ajax=1 taxonomies=product_cat:8]' );
 ?>
 
