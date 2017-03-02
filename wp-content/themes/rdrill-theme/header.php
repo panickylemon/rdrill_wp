99df06fa41e3dd56922775f48e2f2251b53528a5
panickylemon <panickylemon>
2017-03-02 14:30:44 +0300

    category

--- wp-content/themes/rdrill-theme/header.php
+++ wp-content/themes/rdrill-theme/header.php
@@ -2,7 +2,11 @@
 <html lang="ru">
 <head>
 	<meta charset="UTF-8">
-
+	<title>
+		<?php
+		wp_title();
+		?>
+	</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="icon" href="/wp-content/themes/rdrill-theme/image/icons/favicon.ico" type="image/x-icon">
 	<?php wp_head(); ?>
