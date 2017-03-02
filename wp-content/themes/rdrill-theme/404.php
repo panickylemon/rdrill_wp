f94426d7f953dccdf9ffe70f9b51a7a9d4958f49
panickylemon <panickylemon>
2017-03-02 15:38:47 +0300

    category

--- wp-content/themes/rdrill-theme/404.php
+++ wp-content/themes/rdrill-theme/404.php
@@ -8,11 +8,9 @@
     <div class="contacts-wrap">
         <div class="l-container">
 
-            <h1><?php the_title(); ?></h1>
+            <h1>Страница не найдена</h1>
 
-            <?php if (have_posts()): while (have_posts()): the_post(); ?>
-                <?php the_content(); ?>
-            <?php endwhile; endif; ?>
+           <p>К сожалению, указанная страница на сайте отсутствует.</p>
 
         </div>
     </div>
