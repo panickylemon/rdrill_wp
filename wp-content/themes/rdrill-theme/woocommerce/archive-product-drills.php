6d6fa6ce5dbdac5b67ef921136f78e0ad2b8f3fa
panickylemon <alenka__1992@mail.ru>
2017-03-02 22:37:15 +0300

    edit

--- wp-content/themes/rdrill-theme/woocommerce/archive-product-drills.php
+++ wp-content/themes/rdrill-theme/woocommerce/archive-product-drills.php
@@ -64,13 +64,13 @@ do_action('woocommerce_before_main_content');
     <div class="l-container">
         <?php
         echo do_shortcode( '[woof sid="auto_shortcode" tax_only="product_cat,pa_vyisota" autohide=0 autosubmit=0
-        taxonomies=product_cat:8 ajax_redraw=0]' );
+        taxonomies=product_cat:8]' );
         ?>
         <?php dynamic_sidebar( 'sidebar-1' ); ?>
 
 <?php
 
-echo do_shortcode( '[woof_products per_page=8 columns=3 is_ajax=1 taxonomies=product_cat:8]' );
+echo do_shortcode( '[woof_products per_page=1 columns=3 is_ajax=1 taxonomies=product_cat:8]' );
 ?>
 
         <div class="button-show-filter"><span>Показать фильтр</span></div>
