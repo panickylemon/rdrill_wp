1c2aa31c47f90c32e20f1267cc3d972d67938682
panickylemon <panickylemon>
2017-03-02 16:21:22 +0300

    category

--- wp-content/themes/rdrill-theme/woocommerce/archive-product-drills.php
+++ wp-content/themes/rdrill-theme/woocommerce/archive-product-drills.php
@@ -64,7 +64,7 @@ do_action('woocommerce_before_main_content');
     <div class="l-container">
         <?php
        // echo do_shortcode( '[woof sid="auto_shortcode" autohide=0 autosubmit=0 taxonomies=product_cat:8 ]' );
-        echo do_shortcode( '[Contact_Form_Builder id="2"]' );
+        echo do_shortcode( '[wd_contact_form id="12"]' );
 
         ?>
         <?php dynamic_sidebar( 'sidebar-1' ); ?>
