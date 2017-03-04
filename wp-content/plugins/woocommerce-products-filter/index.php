05730647ff10fa7ed05dd07071757b496a96f178
panickylemon <alenka__1992@mail.ru>
2017-03-04 16:21:36 +0300

    edit

--- wp-content/plugins/woocommerce-products-filter/index.php
+++ wp-content/plugins/woocommerce-products-filter/index.php
@@ -2181,14 +2181,14 @@ final class WOOF {
                         woocommerce_product_loop_end();
                     }
                     ?>
-
+                    <div class="pagination-wrap-2">
                     <?php
                     if ($show_loop_filters)
                     {
                         do_action('woocommerce_after_shop_loop');
                     }
                     ?>
-
+                    </div>
                 </div>
 
 
