4483c24fcaf31f6524dc260e49b97e00db6db3b7
panickylemon <alenka__1992@mail.ru>
2017-03-04 16:48:30 +0300

    edit

--- wp-content/plugins/woocommerce-products-filter/views/woof.php
+++ wp-content/plugins/woocommerce-products-filter/views/woof.php
@@ -530,7 +530,8 @@ if (!function_exists('woof_print_item_by_key'))
                                 <button class="base-button base-button--red button-filter woof_submit_search_form">Показать</button>
 
 		                        <button class="base-button base-button--grey button-filter button-filter-reset" data-link="<?php echo
-		                                    $woof_link ?>">Сбросить</button>
+		                                    $woof_link ?>"><a href="<?php echo
+		                                    $woof_link ?>">Сбросить</a></button>
 		                    <?php endif; ?>
                         </div>
 
