195755dbdcbb4f64c04a770e5b4a1c7d49fec809
panickylemon <alenka__1992@mail.ru>
2017-03-04 17:10:47 +0300

    edit

--- wp-content/themes/rdrill-theme/woocommerce/content-product-drills.php
+++ wp-content/themes/rdrill-theme/woocommerce/content-product-drills.php
@@ -36,7 +36,7 @@ if ( empty( $product ) || ! $product->is_visible() ) {
         <p class="catalog-product__title"><span><?php echo get_the_title() ?></span></p>
         <div itemprop="description" class="catalog-product__description">
             <?php $excerpt = apply_filters( 'woocommerce_short_description', $post->post_excerpt );
-                echo substr('1234567890qwe', 0, 10);
+                echo substr($excerpt, 0, 10);
                 echo $excerpt;
             ?>
             <?php //echo apply_filters( 'woocommerce_short_description', $post->post_excerpt ) ?>
