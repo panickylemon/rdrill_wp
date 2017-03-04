0a6e7280f5f94e2f8dcd9ae791ee2db3b04c5e83
panickylemon <alenka__1992@mail.ru>
2017-03-05 01:04:57 +0300

    edit

--- wp-content/themes/rdrill-theme/woocommerce/single-product/review.php
+++ wp-content/themes/rdrill-theme/woocommerce/single-product/review.php
@@ -25,7 +25,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div itemprop="review" itemscope itemtype="http://schema.org/Review" <?php comment_class(); ?> id="li-comment-<?php
 comment_ID() ?>">
 
-	<div id="comment-<?php comment_ID(); ?>" class="comment_container">
+	<div id="comment-<?php comment_ID(); ?>" class="comment_container review-item">
 
 		<?php
 		/**
