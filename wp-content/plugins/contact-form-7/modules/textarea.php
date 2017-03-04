65eee7a9e233192a808be967a7ddba67cebf9ec2
panickylemon <alenka__1992@mail.ru>
2017-03-04 21:07:32 +0300

    edit

--- wp-content/plugins/contact-form-7/modules/textarea.php
+++ wp-content/plugins/contact-form-7/modules/textarea.php
@@ -73,8 +73,7 @@ function wpcf7_textarea_form_tag_handler( $tag ) {
 	$atts = wpcf7_format_atts( $atts );
 
 	$html = sprintf(
-		'<span class="wpcf7-form-control-wrap %1$s"><textarea %2$s>%3$s</textarea>%4$s</span>',
-		sanitize_html_class( $tag->name ), $atts,
+		'<textarea %1$s>%2$s</textarea>%3$s', $atts,
 		esc_textarea( $value ), $validation_error );
 
 	return $html;
