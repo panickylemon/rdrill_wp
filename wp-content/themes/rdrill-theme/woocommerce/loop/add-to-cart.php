942504fc5b43697f4a8f7dd967553ce16e35ad3c
panickylemon <alenka__1992@mail.ru>
2017-03-04 16:03:35 +0300

    edit

--- wp-content/themes/rdrill-theme/woocommerce/loop/add-to-cart.php
+++ wp-content/themes/rdrill-theme/woocommerce/loop/add-to-cart.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * The template for displaying product content within loops
+ * Loop Add to Cart
  *
- * This template can be overridden by copying it to yourtheme/woocommerce/content-product.php.
+ * This template can be overridden by copying it to yourtheme/woocommerce/loop/add-to-cart.php.
  *
  * HOWEVER, on occasion WooCommerce will need to update template files and you
  * (the theme developer) will need to copy the new files to your theme to
@@ -10,32 +10,25 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see     https://docs.woocommerce.com/document/template-structure/
- * @author  WooThemes
- * @package WooCommerce/Templates
- * @version 2.6.1
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     2.5.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit;
 }
 
 global $product;
 
-// Ensure visibility
-if ( empty( $product ) || ! $product->is_visible() ) {
-	return;
-}
-?>
-
-<div class="catalog-product">
-	<a href="<?php echo get_the_permalink() ?>" class="catalog-product__content">
-		<div class="catalog-product__img">
-			<?php do_action( 'woocommerce_before_shop_loop_item_title' ); ?>
-		</div>
-		<p class="catalog-product__title"><span><?php echo get_the_title() ?></span></p>
-		<div itemprop="description" class="catalog-product__description">
-			<?php echo apply_filters( 'woocommerce_short_description', $post->post_excerpt ) ?>
-		</div>
-	</a>
-</div>
+echo apply_filters( 'woocommerce_loop_add_to_cart_link',
+	sprintf( '<a rel="nofollow" href="%s" data-quantity="%s" data-product_id="%s" data-product_sku="%s" class="%s">%s</a>',
+		esc_url( $product->add_to_cart_url() ),
+		esc_attr( isset( $quantity ) ? $quantity : 1 ),
+		esc_attr( $product->id ),
+		esc_attr( $product->get_sku() ),
+		esc_attr( isset( $class ) ? $class : 'button' ),
+		esc_html( $product->add_to_cart_text() )
+	),
+$product );
