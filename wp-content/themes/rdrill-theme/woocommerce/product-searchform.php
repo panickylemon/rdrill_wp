5591ef45ba9114a8295409b273921174fed09f31
panickylemon <alenka__1992@mail.ru>
2017-03-04 18:41:13 +0300

    edit

--- wp-content/themes/rdrill-theme/woocommerce/product-searchform.php
+++ wp-content/themes/rdrill-theme/woocommerce/product-searchform.php
@@ -25,7 +25,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <form role="search" method="get" class="header__search woocommerce-product-search" action="<?php echo esc_url(
 		home_url( '/'  ) ); ?>">
-	<label class="screen-reader-text" for="woocommerce-product-search-field"><?php _e( 'Search for:', 'woocommerce' ); ?></label>
+	<label style="display: none;" class="screen-reader-text" for="woocommerce-product-search-field"><?php _e( 'Search
+	for:',
+				'woocommerce'
+		); ?></label>
 	<input type="search" id="woocommerce-product-search-field" class="search-field b-search__expand" placeholder="Поиск" value="<?php
 	echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'woocommerce' ); ?>" />
 	<input class="b-search__button" type="submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'woocommerce' ); ?>" />
