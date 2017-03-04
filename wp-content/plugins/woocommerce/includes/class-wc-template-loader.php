0ab9e70d41422563cb69833b5a6e86d84fe17957
panickylemon <alenka__1992@mail.ru>
2017-03-04 17:46:59 +0300

    edit

--- wp-content/plugins/woocommerce/includes/class-wc-template-loader.php
+++ wp-content/plugins/woocommerce/includes/class-wc-template-loader.php
@@ -70,8 +70,12 @@ class WC_Template_Loader {
 			$find[] = WC()->template_path() . $file;
 
 		} elseif ( is_post_type_archive( 'product' ) || is_page( wc_get_page_id( 'shop' ) ) ) {
+			if ( is_search() ) {
+				$file 	= 'archive-product.php';
+			} else {
+				$file 	= 'archive-catalog.php';
+			}
 
-			$file 	= 'archive-catalog.php';
 			$find[] = $file;
 			$find[] = WC()->template_path() . $file;
 
