5a26fe0ea14faaa32cf60296cbef02a8ae5bc283
panickylemon <panickylemon>
2017-03-07 12:18:20 +0300

    category

--- wp-content/themes/rdrill-theme/woocommerce/taxonomy-product_cat.php
+++ wp-content/themes/rdrill-theme/woocommerce/taxonomy-product_cat.php
@@ -22,7 +22,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 $category = get_queried_object()->term_id;
 $drills = array("8","13","14","15","16","17","18","19","20");
 $machines = array("9");
-$accessories = array("10","21","22","23","24","25","26","27","28","29");
+$accessories = array("10");
+$accessories_nofilter = array("21","22","23","24","25","26","27","28","29");
 $rails = array("11","106","107","108","109","110","111","112","113","114");
 
 if (in_array($category, $drills)) {
@@ -33,6 +34,9 @@ elseif (in_array($category, $machines)) {
 }
 elseif (in_array($category, $accessories)) {
     wc_get_template( 'archive-product-accessories.php' );
+}
+elseif (in_array($category, $accessories_nofilter)) {
+    wc_get_template( 'archive-product-accessories-nofilter.php' );
 }
 elseif (in_array($category, $rails)) {
     wc_get_template( 'archive-product-rails.php' );
