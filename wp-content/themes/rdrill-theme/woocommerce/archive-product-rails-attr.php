4ba268870e331a2cd069c824b76691a49dc949bf
panickylemon <panickylemon>
2017-03-07 13:52:34 +0300

    category

--- wp-content/themes/rdrill-theme/woocommerce/archive-product-rails-attr.php
+++ wp-content/themes/rdrill-theme/woocommerce/archive-product-rails-attr.php
@@ -66,7 +66,7 @@ do_action('woocommerce_before_main_content');
         <div class="filter-sidebar">
             <div class="filter-catalog" id="filter-form">
                 <?php
-                echo do_shortcode( '[woof tax_only="pa_glubina-rails,pa_diametr-sverla" autohide=0 autosubmit=0
+                echo do_shortcode( '[woof tax_only="pa_glubina-rails" autohide=0 autosubmit=0
                     taxonomies=product_cat:11]' );
                 ?>
             </div>
