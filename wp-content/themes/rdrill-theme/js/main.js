a57bea22cdc1f6ce7a7e85e89525776d06236c51
panickylemon <panickylemon>
2017-03-07 14:24:52 +0300

    category

--- wp-content/themes/rdrill-theme/js/main.js
+++ wp-content/themes/rdrill-theme/js/main.js
@@ -154,6 +154,17 @@ $(document).ready(function () {
         });
 
 
+    $('#request').parsley().on('field:validated', function () {
+        var ok = $('.parsley-error').length === 0;
+        $('.bs-callout-info').toggleClass('hidden', !ok);
+        $('.bs-callout-warning').toggleClass('hidden', ok);
+    })
+        .on('form:submit', function () {
+            window.location.href = "/spasibo/";
+        });
+
+
+
     $(":checkbox.sorting-checkbox-input").change(function () {
         if (this.checked) {
             $(this).closest("tr").addClass("model-active");
