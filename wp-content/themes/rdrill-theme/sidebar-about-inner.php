2b5d39e2d136daf74e04b18e587b36e78d034bb0
panickylemon <panickylemon>
2017-03-10 17:15:24 +0300

    category

--- wp-content/themes/rdrill-theme/sidebar-about-inner.php
+++ wp-content/themes/rdrill-theme/sidebar-about-inner.php
@@ -40,6 +40,10 @@
                     Отправить
                 </button>
             </form>
+
+
+            <?php echo do_shortcode( '[contact-form-7 id="407" title="Опрос"]' ); ?>
+
         </div>
     </div>
 
