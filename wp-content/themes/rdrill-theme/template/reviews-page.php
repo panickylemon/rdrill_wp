efb415182d3b819f7f5bae2add4b1f161a6a4650
panickylemon <panickylemon>
2017-04-05 15:21:55 +0300

    edit

--- wp-content/themes/rdrill-theme/template/reviews-page.php
+++ wp-content/themes/rdrill-theme/template/reviews-page.php
@@ -18,6 +18,36 @@
             <?php if (have_posts()): while (have_posts()): the_post(); ?>
                 <?php the_content(); ?>
             <?php endwhile; endif; ?>
+            <div class="news-slider">
+                <div id="reviews-slider" class="owl-carousel owl-theme">
+                    <div class="news-slider__item">
+                        <a href="/wp-content/themes/rdrill-theme/image/reviews/1-otziv-rotabroach.jpg" data-lightbox="images">
+                            <img src="/wp-content/themes/rdrill-theme/image/reviews/1-otziv-rotabroach.jpg" alt="">
+                        </a>
+                    </div>
+
+                    <div class="news-slider__item">
+                        <a href="/wp-content/themes/rdrill-theme/image/reviews/2-otziv-rotabroach.jpg" data-lightbox="images">
+                            <img src="/wp-content/themes/rdrill-theme/image/reviews/2-otziv-rotabroach.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="news-slider__item">
+                        <a href="/wp-content/themes/rdrill-theme/image/reviews/3-otziv-rotabroach.jpg" data-lightbox="images">
+                            <img src="/wp-content/themes/rdrill-theme/image/reviews/3-otziv-rotabroach.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="news-slider__item">
+                        <a href="/wp-content/themes/rdrill-theme/image/reviews/4-otziv-rotabroach.jpg" data-lightbox="images">
+                            <img src="/wp-content/themes/rdrill-theme/image/reviews/4-otziv-rotabroach.jpg" alt="">
+                        </a>
+                    </div>
+                    <div class="news-slider__item">
+                        <a href="/wp-content/themes/rdrill-theme/image/reviews/5-otziv-rotabroach.jpg" data-lightbox="images">
+                            <img src="/wp-content/themes/rdrill-theme/image/reviews/5-otziv-rotabroach.jpg" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </main>
